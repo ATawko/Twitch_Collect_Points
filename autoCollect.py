e44aaa4fe0ffae3b62ea014578b3cac6daa068dd
ATawko <11181470+ATawko@users.noreply.github.com>
2019-09-15 19:26:56 -0300

    Added admin check - printing execution times

--- autoCollect.py
+++ autoCollect.py
@@ -1,18 +1,31 @@
+from checkAdmin import *
 from imagesearch import *
-from ctypes import *
 from PIL import Image
 
 #Editable Variables
 iteration = 0
 wait_time = 60
-imageNames = ["./twitch_points.png"]
+imageNames = ["./images/twitch_points.png", "./images/test.png"]
 imagePrecision = 0.8
+
+userAdmin = is_user_admin()
+
+if not userAdmin:
+    print('Please Run As Admin To Enable Mouse Lock on Click')
+    print()
 
 while True:
     iteration += 1
     imgInfo, sizes = [], []
+    startTime = time.time() #Iteration Start Time
 
-    print(f'Iteration: {iteration}')    
+    print(f'Iteration: {iteration}')
+    if len(imageNames) > 1: 
+        print("Images to Search: " + str(len(imageNames)))   
+
+
+    #Image Search Start Time
+    imageSearchStart = time.time()
 
     #Find Images
     for img in imageNames:
@@ -25,34 +38,56 @@ while True:
 
         #Create Object For Each Image Found
         imgInfo.append( {"pos" : imgPos, "width" : width, "height" : height} )
-        
+
+    #Image Search End time
+    print()
+    print("Time to Search For Image(s): " + str(round(time.time() - imageSearchStart,2)) + " Seconds")
+    print()
+
 
     #Click Images
+    currrentImageNumber = 1
     for img in imgInfo:
 
         if img['pos'][0] != -1:
-            print("Found Image")
+            imageClickStart = time.time() #Image Click Start Time
+            print("Found Image. " + str(currrentImageNumber) + "/" + str(len(imgInfo)))
 
-            print("Locking Mouse Temporarily & Clicking Icon")
-            ok = windll.user32.BlockInput(True) #enable block
+            if userAdmin:
+                print("Locking Mouse Temporarily & Clicking Icon")
+                status = windll.user32.BlockInput(True) #enable block
 
-            #Click Icon
-            userPOS = pyautogui.position()
-            pyautogui.moveTo(img['pos'][0] + (width/2), img['pos'][1] + (height/2))
-            pyautogui.click()
-            pyautogui.moveTo(userPOS)
+                #Click Icon
+                userPOS = pyautogui.position()
+                pyautogui.moveTo(img['pos'][0] + (img['width']/2), img['pos'][1] + (img['height']/2))
+                pyautogui.click()
+                pyautogui.moveTo(userPOS)
+            
+                #Restore Mouse Access
+                status = windll.user32.BlockInput(False) #disable block 
+                print("Restored Mouse Access & Location")
+            else:
+                #Click Icon
+                userPOS = pyautogui.position()
+                pyautogui.moveTo(img['pos'][0] + (img['width']/2), img['pos'][1] + (img['height']/2))
+                pyautogui.click()
+                pyautogui.moveTo(userPOS)
 
-            #Restore Mouse Access
-            ok = windll.user32.BlockInput(False) #disable block 
-            print("Restored Mouse Access & Location")
+
+            print("Time to Click: " + str(round(time.time() - imageClickStart, 2)) + " Second(s)")
+            print()
+            currrentImageNumber += 1 
 
         else:
-            print("Image Not Found")
-
- 
-        
+            print("Image Not Found. " + str(currrentImageNumber) + "/" + str(len(imgInfo)))
+            print()
+            currrentImageNumber += 1 
     
+
+    print("Total Iteration Time: " + str(round(time.time() - startTime, 2)))
     print(f"Pausing for {wait_time} seconds.")
+    print()
+    print("--------------------------------")
     print()
     time.sleep(wait_time)
 
